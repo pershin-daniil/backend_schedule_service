86659f1fe391b8ad7d76ea65db8a74bbb6e68b9b
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-12-24 19:15:42 +0000

    feat: add endpoints

--- server.go
+++ server.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func timeHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,15 +18,51 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
+	stateTime = append(stateTime, time.Now())
+	_, err := fmt.Fprintf(w, "%s\n", stateTime[len(stateTime)-1])
+	if err != nil {
+		log.Panic(err)
+	}
+}
+func timeHistoryHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/timeHistory" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
 
-	_, err := fmt.Fprintf(w, "%s\n", time.Now())
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	_, err := fmt.Fprintf(w, "%v\n", stateTime)
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
+func resetTimeHistoryHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/resetTimeHistory" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	_, err := fmt.Fprintf(w, "%v\n", stateTime)
+	if err != nil {
+		log.Panic(err)
+	}
+	stateTime = make([]time.Time, 0)
+}
+
+var stateTime = make([]time.Time, 0)
+
 func main() {
 	http.HandleFunc("/timeNow", timeHandler)
+	http.HandleFunc("/timeHistory", timeHistoryHandler)
+	http.HandleFunc("/resetTimeHistory", resetTimeHistoryHandler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
